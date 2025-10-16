77bc2bb46e79c73b3de7699cbb9c4f6a8c38f535
ishanrana363 <ishanrana363@gmail.com>
2025-10-16 16:15:08 +0600

    next config file live link added

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'standalone',
   images: {
-    domains: ["ui-avatars.com", "10.10.10.55"], // add any external host you use
+    domains: ["ui-avatars.com", "http://api.66.29.135.26.nip.io"], // add any external host you use
   },
 };
 
