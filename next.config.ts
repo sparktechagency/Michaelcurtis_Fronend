ef75104710c8de61c6a5699f743ff304a07cbe7c
Kazi Omar Faruk <kaziomar144@gmail.com>
2025-10-19 12:57:46 +0600

    change next js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
-    domains: ['api.66.29.135.26.nip.io', 'ui-avatars.com'],
+    domains: ['api.66.29.135.26.nip.io', 'ui-avatars.com', 'api.coveragegrader.com'],
   },
 };
 
