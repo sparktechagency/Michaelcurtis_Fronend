ab1249ae98a3ec3454032c8d661ef068caa76ba5
ishanrana363 <ishanrana363@gmail.com>
2025-12-11 09:26:01 +0600

    react latest package install

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'standalone',
   output: 'standalone',
   images: {
-    domains: ['api.66.29.135.26.nip.io', 'ui-avatars.com', 'api.coveragegrader.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.66.29.135.26.nip.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ui-avatars.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.coveragegrader.com',
+      },
+    ],
   },
 };
 
